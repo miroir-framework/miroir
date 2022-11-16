eda8877b5b10d6a49221382c788e319ceac4ffec
Miroir Framework <miroir-framework@protonmail.com>
2022-11-16 16:12:41 +0100

    importing json files, added Entity and Report entity definitions.

--- miroir-standalone-app/src/index.tsx
+++ miroir-standalone-app/src/index.tsx
@@ -2,10 +2,14 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import FirstComponent from './components/FirstComponent'
 import UserComponent from "./components/UserComponent";
+import entityEntity from "./entities/Entity.json";
+import reportEntity from "./entities/Report.json";
+
 ReactDOM.render(
     <div>
       <h1>Hello, Welcome to React and TypeScript</h1>
-      <FirstComponent/>
+      {entityEntity["name"]}
+      {/* <FirstComponent/> */}
       <UserComponent name="John Doe" age={26} address="87 Summer St, Boston, MA 02110" dob={new Date()} />
     </div>,
     document.getElementById("root")
