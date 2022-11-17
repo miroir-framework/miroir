2b3ea81191f9d1efae5fdbf674c3b805a46590dd
Miroir Framework <miroir-framework@protonmail.com>
2022-11-17 16:34:09 +0100

    TableComponent takes displayed information from props.

--- miroir-standalone-app/src/index.tsx
+++ miroir-standalone-app/src/index.tsx
@@ -1,12 +1,42 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import entityEntity from "./miroir-fwk/assets/entities/Entity.json";
 import TableComponent from "./miroir-fwk/view/TableComponent"
+import entity from "./miroir-fwk/assets/entities/Entity.json"
+import report from "./miroir-fwk/assets/entities/Report.json"
+
 
 ReactDOM.render(
     <div>
       <h1>Miroir standalone demo app</h1>
-      <TableComponent></TableComponent>
+      <TableComponent
+        columnDefs={
+          entity.attributes.find(a=>a.name==="attributes").attributeFormat.map(
+            // (a)=>{return {"headerName": a.id.toString(), "field": "{a.id}"}}
+            (a)=>{return {"headerName": a.display, "field": a.name}}
+          )
+        }
+        // columnDefs={[
+        //   {"headerName": "Id", "field": "id"},
+        //   {"headerName": "Name", "field": "name"},
+        //   {"headerName": "Display", "field": "display"},
+        //   {"headerName": "Type", "field": "type"}
+        // ]}
+        rowData={report.attributes}
+        // rowData={[
+        //   {
+        //       "id": 1,
+        //       "name": "name",
+        //       "display": "Name",
+        //       "type": "STRING"
+        //   },
+        //   {
+        //       "id": 2,
+        //       "name": "description",
+        //       "display": "Description",
+        //       "type": "STRING"
+        //   },
+        // ]}
+      ></TableComponent>
     </div>,
     document.getElementById("root")
 );
--- miroir-standalone-app/src/miroir-fwk/view/TableComponent.tsx
+++ miroir-standalone-app/src/miroir-fwk/view/TableComponent.tsx
@@ -1,65 +1,59 @@
+import { AgGridReact } from 'ag-grid-react';
 import * as React from "react";
-import {AgGridReact} from 'ag-grid-react';
 
+import { Card, CardContent, CardHeader, Container } from "@mui/material";
 import 'ag-grid-community/styles//ag-grid.css';
 import 'ag-grid-community/styles//ag-theme-alpine.css';
 
-export default class UserComponent extends React.Component<{},{columnDefs:any[],rowData:any[]}> {
-  constructor (props: {} | Readonly<{}>){
+export interface TableComponentProps {
+  columnDefs:{"headerName": string, "field": string}[];
+  rowData:any[];
+};
+
+export default class TableComponent 
+  extends React.Component<
+    TableComponentProps,
+    TableComponentProps
+  > {
+  constructor (props:TableComponentProps){
     super(props);
 
-    this.state = {
-      columnDefs: [
-        {headerName: "Id", field: "id"},
-        {headerName: "Name", field: "name"},
-        {headerName: "Display", field: "display"},
-        {headerName: "Type", field: "type"},
-        
-      ],
-      rowData: [
-        {
-            "id": 1,
-            "name": "name",
-            "display": "Name",
-            "type": "STRING"
-        },
-        {
-            "id": 2,
-            "name": "description",
-            "display": "Description",
-            "type": "STRING"
-        },
-      ]
-    };
+    this.state = props;
   }
   render() {
-    // const gridOptions = {
-    //   rowData: null,
-    //   columnDefs: this.state.columnDefs,
-    //   defaultColDef: {
-    //     editable: true,
-    //     sortable: true,
-    //     filter: true,
-    //     resizable: true,
-    //   },
-    // };
-
     return (
-      // height: '500px',
-      <div
-        className="ag-theme-alpine"
-        style={
-          {
-            height: '500px',
-            width: '600px'
-          }
-        }
-      >
-        <AgGridReact
-          columnDefs={this.state.columnDefs}
-          rowData={this.state.rowData}>
-        </AgGridReact>
-      </div>
+      <Container maxWidth='lg'>
+        <Card>
+          <CardHeader>
+            AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+          </CardHeader>
+          <CardContent>
+              <div
+                className="ag-theme-alpine"
+                style={
+                  {
+                    height: '500px',
+                    width: 'auto'
+                  }
+                }
+              >
+                <AgGridReact
+                  columnDefs={this.state.columnDefs}
+                  rowData={this.state.rowData}
+                  defaultColDef={
+                    {
+                      editable: true,
+                      sortable: true,
+                      filter: true,
+                      resizable: true
+                    }
+                  }
+                >
+                </AgGridReact>
+              </div>
+          </CardContent>
+        </Card>
+      </Container>
     );
   }
 }
