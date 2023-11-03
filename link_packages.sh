e9dd8db32649fc22ad8d3a8da18600a040813575
Miroir Framework <miroir-framework@protonmail.com>
2023-11-03 22:11:31 +0100

    revert

--- link_packages.sh
+++ link_packages.sh
@@ -1,16 +1,16 @@
 cd /var/jenkins_home/workspace/miroir-standalone-app-ci/packages/miroir-core/
-sudo npm link
+npm link
 cd /var/jenkins_home/workspace/miroir-standalone-app-ci/packages/miroir-redux/
-sudo npm link
+npm link
 cd /var/jenkins_home/workspace/miroir-standalone-app-ci/packages/miroir-react/
-sudo npm link
+npm link
 cd /var/jenkins_home/workspace/miroir-standalone-app-ci/packages/miroir-store-indexedDb/
-sudo npm link
+npm link
 cd /var/jenkins_home/workspace/miroir-standalone-app-ci/packages/miroir-store-postgres/
-sudo npm link
+npm link
 cd /var/jenkins_home/workspace/miroir-standalone-app-ci/packages/miroir-standalone-app/
-sudo npm link miroir-core
-sudo npm link miroir-redux
-sudo npm link miroir-react
-sudo npm link miroir-store-indexedDb
-sudo npm link miroir-store-postgres
+npm link miroir-core
+npm link miroir-redux
+npm link miroir-react
+npm link miroir-store-indexedDb
+npm link miroir-store-postgres
