f621c8b86f193ee9e368790f4e71a8adcbef9434
Miroir Framework <miroir-framework@protonmail.com>
2023-11-04 15:38:37 +0100

    ci build: adding tests to jenkins pipeline

--- link_packages.sh
+++ link_packages.sh
@@ -1,6 +1,7 @@
 cd /home/tmp/miroir-standalone-app-ci/packages/miroir-core/ && npm link
 cd /home/tmp/miroir-standalone-app-ci/packages/miroir-redux/  && npm link
 cd /home/tmp/miroir-standalone-app-ci/packages/miroir-react/  && npm link
+cd /home/tmp/miroir-standalone-app-ci/packages/miroir-store-filesystem/  && npm link
 cd /home/tmp/miroir-standalone-app-ci/packages/miroir-store-indexedDb/  && npm link
 cd /home/tmp/miroir-standalone-app-ci/packages/miroir-store-postgres/ && npm link
 cd /home/tmp/miroir-standalone-app-ci/packages/miroir-standalone-app/ && npm link miroir-core
