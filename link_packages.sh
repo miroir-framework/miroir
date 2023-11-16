048cdd55b51844d58c2386a3b58e345f33f155af
Miroir Framework <miroir-framework@protonmail.com>
2023-11-16 12:50:35 +0100

    Jenkinsfile: trying to speed-up dev (not production) build

--- link_packages.sh
+++ link_packages.sh
@@ -1,11 +1,13 @@
 cd "${target_build_dir}/packages/miroir-core/" && npm link
 cd "${target_build_dir}/packages/miroir-localcache-redux/"  && npm link
 cd "${target_build_dir}/packages/miroir-react/"  && npm link
+cd "${target_build_dir}/packages/miroir-server-msw-stub/"  && npm link
 cd "${target_build_dir}/packages/miroir-store-filesystem/"  && npm link
 cd "${target_build_dir}/packages/miroir-store-indexedDb/"  && npm link
 cd "${target_build_dir}/packages/miroir-store-postgres/" && npm link
 cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-core
 cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-localcache-redux
 cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-react
+cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-server-msw-stub
 cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-store-indexedDb
 cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-store-postgres
