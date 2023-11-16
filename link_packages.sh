92c175a273fe461ddf4981c2bca3dc828c60e0bc
Miroir Framework <miroir-framework@protonmail.com>
2023-11-16 12:27:48 +0100

    Jenkinsfile: trying to speed-up dev (not production) build

--- link_packages.sh
+++ link_packages.sh
@@ -1,11 +1,11 @@
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-core/ && npm link
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-localcache-redux/  && npm link
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-react/  && npm link
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-store-filesystem/  && npm link
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-store-indexedDb/  && npm link
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-store-postgres/ && npm link
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-standalone-app/ && npm link miroir-core
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-standalone-app/ && npm link miroir-localcache-redux
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-standalone-app/ && npm link miroir-react
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-standalone-app/ && npm link miroir-store-indexedDb
-cd /home/tmp/miroir-standalone-app-ci/packages/miroir-standalone-app/ && npm link miroir-store-postgres
+cd "${target_build_dir}/packages/miroir-core/" && npm link
+cd "${target_build_dir}/packages/miroir-localcache-redux/"  && npm link
+cd "${target_build_dir}/packages/miroir-react/"  && npm link
+cd "${target_build_dir}/packages/miroir-store-filesystem/"  && npm link
+cd "${target_build_dir}/packages/miroir-store-indexedDb/"  && npm link
+cd "${target_build_dir}/packages/miroir-store-postgres/" && npm link
+cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-core
+cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-localcache-redux
+cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-react
+cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-store-indexedDb
+cd "${target_build_dir}/packages/miroir-standalone-app/" && npm link miroir-store-postgres
