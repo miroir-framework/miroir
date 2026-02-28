c69f709b4a17425c530c87e22d013ba9fe9b5e0c
Miroir Framework <miroir-framework@protonmail.com>
2026-02-28 19:25:15 +0100

    fix double bundling of codemirror in miroir-react and miroir-standalone-app

--- packages/miroir-react/tsup.config.js
+++ packages/miroir-react/tsup.config.js
@@ -8,7 +8,17 @@ export default defineConfig({
   sourcemap: true,
   treeshake: true,
   minify: true,
-  external: ["react", "react-dom", "@mui/material", "@emotion/react", "@emotion/styled"],
+  external: [
+    "react",
+    "react-dom",
+    "@mui/material",
+    "@emotion/react",
+    "@emotion/styled",
+    "@codemirror/state",
+    "@codemirror/view",
+    "@codemirror/lang-javascript",
+    "@uiw/react-codemirror",
+  ],
   esbuildOptions(options) {
     options.jsx = "automatic";
   },
