109dc2b72850e06fa9e58249b8bb65d02e8db736
Miroir Framework <miroir-framework@protonmail.com>
2026-05-05 11:42:49 +0200

    #128 RELEASE: release Miroir Studio v 0.5.0 (and preparation work). Preparing docker image build

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #docker run --name mycontainer --rm --entrypoint="" -it miroir-framework/ci-builder:latest sh
+#docker run --name mycontainer --rm --entrypoint="" -it miroir-framework/miroir-server:latest sh
 #docker run --name mycontainer --rm --entrypoint="" myimage echo "Hello, World!"
 
-# docker build --no-cache --rm -t miroir-framework/ci-workspace-dev:latest ci-workspace-dev
+#   docker run -p 3080:3080 -v /mnt/c/miroir-data:/data miroir-framework/miroir:latest
+
+docker build --no-cache --rm -t miroir-framework/ci-workspace-dev:latest ci-workspace-dev
 docker build --no-cache --rm -t miroir-framework/ci-builder:latest ci-builder
+docker build --no-cache --rm -t miroir-framework/miroir-server:latest miroir-server
